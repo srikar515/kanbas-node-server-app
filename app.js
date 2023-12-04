156ab3b1db93b788648e63d0e706114da3a42979
Nallapu Srikar <nallapusrikar@Nallapus-MacBook-Air.local>
2023-12-04 17:22:03 -0500

    Added mongo

--- app.js
+++ app.js
@@ -5,9 +5,37 @@ import CourseRoutes from './Courses/routes.js';
 import cors from 'cors';
 import ModuleRoutes from "./Modules/routes.js";
 import "dotenv/config";
+import mongoose from 'mongoose';
+import UserRoutes from './users/routes.js';
+import session from 'express-session';
+
 const app = express();
-app.use(cors())
-app.use(express.json())
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+const sessionOptions = {
+  secret: 'any string',
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== 'development') {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: 'none',
+    secure: true,
+  };
+}
+
+app.use(session(sessionOptions));
+
+app.use(express.json());
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
+UserRoutes(app);
 ModuleRoutes(app)
 CourseRoutes(app)
 Lab5(app);
